532879b7e68e62982813568095622429f826e38e
vladimir1970kostricyn <130578214+vladimir1970kostricyn@users.noreply.github.com>
2023-05-14 12:00:54 +0300

    Add files via upload

---
+++ praktika10/main.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from main_new_Kr_Drozdova import one_func
+from main_Nikita_ivanchenko import two_func
+from Elena_Duragina import func_3
+from main_tim import func_4
+from main_vladimir import func_5
+
+st.title("Программная инженерия. Практическая работа 10")
+
+func = st.selectbox(
+    label="Выберите работу:",
+    options=["Первый", "Второй", "Третий", "четвёртый", "пятый"]
+)
+
+
+
+if func == "Первый":
+    st.title("Дроздова Кристина -13 вариант")
+    one_func()
+elif func == "Второй":
+    st.title("вариант 15, Иванченко Никита")
+    two_func()
+elif func == "Третий":
+    st.title("Дюрягина Елена - 14 вариант")
+    func_3()
+elif func == "четвёртый":
+    st.title("Кишев Тимур - 1 вариант")
+    func_4()
+elif func == "пятый":
+    st.title("Кострицын Владимир 9 вариант")
+    func_5()
+
+
+
