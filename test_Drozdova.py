c3d947d9b7701219ba2b2f39fb6f79c379eae20c
Kristinadrozdova <130238303+Kristinadrozdova@users.noreply.github.com>
2023-05-29 17:56:27 +0300

    Add files via upload

---
+++ test_Drozdova.py
@@ -0,0 +1,21 @@
+from fasfas import *
+
+
+def test_result1():
+    data = ["0,0,1,0,0,male", "0,0,1,0,0,female"]
+    assert result1(data) == (1, 1)
+
+
+def test_result2():
+    data = ["0,0,2,0,0,male", "0,0,2,0,0,female"]
+    assert result2(data) == (1, 1)
+
+
+
+
+def test_result3():
+    data = ["0,0,3,0,0,male", "0,0,3,0,0,female"]
+    assert result3(data) == (1, 1)
+
+
+
