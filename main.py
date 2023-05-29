ec4e3cea669b18bca92e58346f010cd254473197
LankiSer <133566219+LankiSer@users.noreply.github.com>
2023-05-29 15:48:17 +0300

    Update main.py
    афы

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from main_new_Kr_Drozdova import one_func
 from main_Nikita_ivanchenko import two_func
 from elena_duragina import func_3
 from main_tim import func_4
-from main_vladimir import func_5
+
 
 st.title("Программная инженерия. Практическая работа 10")
 
