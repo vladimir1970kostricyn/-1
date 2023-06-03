1164c4262dd8d33f2e2e7ab7707770b89fc21193
vladimir1970kostricyn <v.kostritsyn@yandex.ru>
2023-06-03 22:02:15 +0300

    tests

--- test_tim.py
+++ test_tim.py
@@ -1,22 +1,28 @@
-from main_tim import *
+from main_tim import result, result2, result3
 
 
 def test1():
-    data = ['0,1,0,kirill,0,male,"6"', '0,1,0,kirill,0,male,"6"', '0,1,0,kirill,0,male,"6"', '0,1,0,kirill,0,male,"6"',
+    data = ['0,1,0,kirill,0,male,"6"',
+            '0,1,0,kirill,0,male,"6"',
+            '0,1,0,kirill,0,male,"6"',
+            '0,1,0,kirill,0,male,"6"',
             '0,1,0,kirill,0,male,"6"']
     assert result(data) == ['kirill', 'kirill', 'kirill', 'kirill', 'kirill']
 
 
 def test2():
-    data = ['0,1,0,kirill,0,male,"6"', '0,1,0,kirill,0,male,"6"', '0,1,0,kirill,0,male,"6"', '0,1,0,kirill,0,male,"6"',
+    data = ['0,1,0,kirill,0,male,"6"',
+            '0,1,0,kirill,0,male,"6"',
+            '0,1,0,kirill,0,male,"6"',
+            '0,1,0,kirill,0,male,"6"',
             '0,1,0,kirill,0,male,"6"']
     assert result2(data) == ['male', 'male', 'male', 'male', 'male']
 
 
 def test3():
-    data = ['0,1,0,kirill,0,male,"6"', '0,1,0,kirill,0,male,"6"', '0,1,0,kirill,0,male,"6"', '0,1,0,kirill,0,male,"6"',
+    data = ['0,1,0,kirill,0,male,"6"',
+            '0,1,0,kirill,0,male,"6"',
+            '0,1,0,kirill,0,male,"6"',
+            '0,1,0,kirill,0,male,"6"',
             '0,1,0,kirill,0,male,"6"']
     assert result3(data) == ['"6"', '"6"', '"6"', '"6"', '"6"']
-
-
-
